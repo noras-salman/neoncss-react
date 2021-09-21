64144b6f1ddb896d63fcdbc90b65f58606d8eaff
noras <noras.salman@live.com>
2021-09-21 02:35:21 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { RouterLayout } from "./layout/RouterLayout";
+function App() {
+  return (
+    <div className="App">
+      <RouterLayout></RouterLayout>
+    </div>
+  );
+}
+
+export default App;
